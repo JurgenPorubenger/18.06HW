433339e472972c2eaa5d132217d55378f9c4f940
Eugene <jurgen.porubenger@gmail.com>
2019-06-21 17:38:50 +0300

    last1

--- script.js
+++ script.js
@@ -6,42 +6,49 @@
 
 
 let elem  = document.querySelector(".block");
-let i = document.querySelector('.inp');
-let b = document.querySelector('.but');
-// let fafa = document.querySelector('.fafa');
-// let gaga = document.querySelector('.gaga');
+let mainInput = document.querySelector('.inp');
+let mainButton = document.querySelector('.but');
 
-
-
-
-b.addEventListener('click', function () {
-
-    let y = i.value;
-
+mainButton.addEventListener('click', function () {
+    let y = mainInput.value;
     let div1 = document.createElement("div");
     let p1 = document.createElement("p");
     let btn = document.createElement("button");
     // div1.insertBefore(btn,div1.children[0]);
-    div1.classList.add("d1");
-    div1.appendChild(btn);
-    div1.appendChild(p1);
-    p1.innerHTML=i.value;
-    if (div1.contains(p1)) {
-        if(p1) {
-            let arr = (p1.split(''));
-            console.log(arr);
+    function divAppendChildAndClass() {
+        div1.classList.add("d1");
+        let allDiv= document.querySelectorAll(".d1");
+        div1.appendChild(btn);
+        div1.appendChild(p1);
+        p1.innerHTML=y;
+        elem.appendChild(div1);
+        let allDivLenght = allDiv.length;
+        let pInDiv1 = div1.querySelector('p');
+        pInDiv1.innerHTML= pInDiv1.innerHTML+ ""+allDivLenght;
+        for (let i = 0; i<allDivLenght; i++) {
+       
         }
-
-
     }
-
-    elem.appendChild(div1);
-    let btnn = div1.querySelector('button');
-    btnn.classList.add("btn2");
-    btnn.addEventListener('click', function () {
+    divAppendChildAndClass();
+    let smallButton = div1.querySelector('button');
+    smallButton.classList.add("btn2");
+    smallButton.addEventListener('click', function () {
         elem.removeChild(div1);
+        
     })
-    console.log(btnn);
+    
+    // if (div1.contains(p1)) {
+    //     if(p1) {
+    //         let arr = (p1.split(''));
+    //         console.log(arr);
+    //     }
+
+
+    // }
+
+
+    
+    // console.log(btnn);
     // elem.insertBefore(h1,elem.children[0]);
     // fafa.removeChild(gaga);
 });
